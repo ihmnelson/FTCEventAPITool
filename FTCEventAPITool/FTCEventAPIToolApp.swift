09d9f823a00a99289934c36898a80f0700ee59ac
Isaac Nelson <ihmnelson@outlook.com>
2023-01-23 18:48:02 -0800

    Initial Commit

---
+++ FTCEventAPITool/FTCEventAPIToolApp.swift
@@ -0,0 +1,17 @@
+//
+//  FTCEventAPIToolApp.swift
+//  FTCEventAPITool
+//
+//  Created by Isaac Nelson on 1/23/23.
+//
+
+import SwiftUI
+
+@main
+struct FTCEventAPIToolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
